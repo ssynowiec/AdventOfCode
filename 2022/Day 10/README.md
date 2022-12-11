23a95669fe2e1f700157e5d4254eef5d1c1e83e0
ssynowiec <stasyn1410@gmail.com>
2022-12-11 12:36:32 +0100

    Day 10 - part two, 2022

--- 2022/Day 10/README.md
+++ 2022/Day 10/README.md
@@ -5,7 +5,7 @@
 | Name                                                                                       | Correctness |
 | :----------------------------------------------------------------------------------------- | :---------- |
 | [Part One](https://github.com/ssynowiec/AdventOfCode/blob/main/2022/Day%20010/part-one.js) | Correct ✅  |
-| [Part Two](https://github.com/ssynowiec/AdventOfCode/blob/main/2022/Day%20010/part-two.js) | in progress |
+| [Part Two](https://github.com/ssynowiec/AdventOfCode/blob/main/2022/Day%20010/part-two.js) | Correct ✅  |
 | [Test](https://github.com/ssynowiec/AdventOfCode/blob/main/2022/Day%20010/test.js)         | Passed ✅   |
 
-### Day completed with ⭐
+### Day completed with ⭐⭐
--- 2022/Day 10/part-two.js
+++ 2022/Day 10/part-two.js
@@ -1,7 +1,7 @@
 const { promises: fsPromises } = require('fs');
 const { dirname } = require('path');
 
-const asyncReadFile = async(filename) => {
+const asyncReadFile = async filename => {
 	try {
 		const contents = await fsPromises.readFile(
 			dirname(__filename) + filename,
@@ -14,12 +14,53 @@ const asyncReadFile = async(filename) => {
 	} catch (err) {
 		console.log(err);
 	}
-}
+};
 
 const partTwo = async input => {
+	let i = 1;
 	const data = await asyncReadFile(input);
 
-	return result
+	const instructions = [];
+	data.forEach(line => {
+		const instruction = line.split(' ');
+		if (instruction[0] === 'addx') {
+			instructions.push([instruction[0], instruction[1]]);
+			instructions.push(['noop']);
+		} else {
+			instructions.push([instruction[0]]);
+		}
+	});
+
+	const cycles = [];
+	let cycle = 0;
+	instructions.forEach((instruction, index) => {
+		if (instruction[0] === 'addx') {
+			cycles[cycle] = i;
+			i += parseInt(instruction[1]);
+			cycles[cycle + 1] = i;
+		} else {
+			cycles[cycle] = i;
+		}
+		cycle++;
+	});
+
+	for (let i = 0; i < 240; i += 40) {
+		let line = '🟥';
+		let lineIndex = 0;
+		for (let j = 0; j < 40; j++, lineIndex++) {
+			const position = cycles[i + j];
+			if (
+				[position - 1, position, position + 1].includes(lineIndex + 1)
+			) {
+				line += '🟥';
+			} else {
+				line += '⬛';
+			}
+		}
+		console.log(line);
+	}
 };
+
+partTwo('/data.txt');
 
 module.exports = { partTwo };
