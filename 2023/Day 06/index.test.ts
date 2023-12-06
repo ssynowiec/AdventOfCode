ae20f63b88d0083d0db5f471cb0914e4b488c6cf
Stanisław Synowiec <stasyn1410@gmail.com>
2023-12-06 08:10:19 +0100

    Day 6 - part two, 2023

--- 2023/Day 06/index.test.ts
+++ 2023/Day 06/index.test.ts
@@ -12,3 +12,15 @@ test('part 1 should return 288', async () => {
 		console.log('Part 1:', value);
 	}
 });
+
+test('part 2 should return 71503', async () => {
+	const result = await partTwo(__dirname + '/test.data.txt');
+	const expectedResult = 71503;
+
+	expect(result).toBe(expectedResult);
+
+	if (result === expectedResult) {
+		const value = await partTwo(__dirname + '/input.data.txt');
+		console.log('Part 2:', value);
+	}
+});
--- 2023/Day 06/part-two.ts
+++ 2023/Day 06/part-two.ts
@@ -9,7 +9,29 @@ export const partTwo = async (input: string) => {
 
 	let result = 0;
 
-	data.forEach(line => {});
+	const times = data[0]
+		.slice(data[0].indexOf(':'), data[0].length)
+		.split(' ')
+		.filter(element => !isNaN(parseInt(element)))
+		.join('');
+	const distances = data[1]
+		.slice(data[1].indexOf(':'), data[1].length)
+		.split(' ')
+		.filter(element => !isNaN(parseInt(element)))
+		.join('');
+
+	const raceTime = parseInt(times);
+	const recordDistance = parseInt(distances);
+
+	for (let i = 0; i < raceTime; i++) {
+		const holdingTime = i;
+		const racingTime = raceTime - holdingTime;
+		const speed = holdingTime;
+		const distance = speed * racingTime;
+		if (distance > recordDistance) {
+			result += 1;
+		}
+	}
 
 	return result;
 };
