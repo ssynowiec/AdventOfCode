d14a805f11d8ed38f3b1780a34454e86a5facfbd
Stanisław Synowiec <stasyn1410@gmail.com>
2025-12-01 22:49:52 +0100

    prepare Day 02 2025

---
+++ 2025/Day 02/index.test.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import { partOne } from './part-one';
+// import { partTwo } from './part-two';
+
+test.describe('Day 02', () => {
+	const expectedResult1 = 0;
+	test(`Part 1 should return ${expectedResult1}`, async () => {
+		const result = partOne(__dirname + '/test.data.txt');
+
+		expect(result).toBe(expectedResult1);
+
+		if (result === expectedResult1) {
+			const value = partOne(__dirname + '/input.data.txt');
+			console.log('Part 1:', value);
+		}
+	});
+
+	// const expectedResult2 = 6;
+	// test(`Part 2 should return ${expectedResult2}`, async () => {
+	// 	const result = partTwo(__dirname + '/test.data.txt');
+
+	// 	expect(result).toBe(expectedResult2);
+
+	// 	if (result === expectedResult2) {
+	// 		const value = partTwo(__dirname + '/input.data.txt');
+	// 		console.log('Part 2:', value);
+	// 	}
+	// });
+});
--- 2025/Day 02/part-two.ts
+++ 2025/Day 02/part-two.ts
@@ -1,6 +1,6 @@
 import { readFile } from '@/helpers/read-file';
 
-export const partOne = (input: string): number => {
+export const partTwo = (input: string): number => {
 	const data = readFile(input);
 
 	let result = 0;
