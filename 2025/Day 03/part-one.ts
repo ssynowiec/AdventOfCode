ab46026ba7807b535df7c6b00021603fc1768ee5
Stanisław Synowiec <stasyn1410@gmail.com>
2025-12-03 06:31:02 +0100

    Day 03 - part one 2025

--- 2025/Day 03/part-one.ts
+++ 2025/Day 03/part-one.ts
@@ -5,7 +5,22 @@ export const partOne = (input: string): number => {
 
 	let result = 0;
 
-	data.forEach(line => {});
+	data.forEach(line => {
+		const ratings = line.split('');
+
+		let max = 0;
+		ratings.forEach((rating, index) => {
+			for (let i = index + 1; i < ratings.length; i++) {
+				const element = ratings[i];
+				const potentialMax = Number(rating + element);
+				if (potentialMax > max) {
+					max = potentialMax;
+				}
+			}
+		});
+
+		result += max;
+	});
 
 	return result;
 };
