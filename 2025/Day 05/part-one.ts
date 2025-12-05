a8fc892c5a4378cb25e0910526975ae9e7f480be
Stanisław Synowiec <stasyn1410@gmail.com>
2025-12-05 06:19:48 +0100

    Day 05 - part one 2025

--- 2025/Day 05/part-one.ts
+++ 2025/Day 05/part-one.ts
@@ -3,9 +3,26 @@ import { readFile } from '@/helpers/read-file';
 export const partOne = (input: string): number => {
 	const data = readFile(input);
 
+	const separateId = data.indexOf('');
+	const ranges = data.slice(0, separateId);
+	const ids = data.slice(separateId + 1).map(Number);
+
 	let result = 0;
 
-	data.forEach(line => {});
+	ids.forEach(id => {
+		let counted = false;
+		ranges.forEach(range => {
+			const [minStr, maxStr] = range.split('-').map(Number);
+			const min = Number(minStr);
+			const max = Number(maxStr);
+
+			if (id >= min && id <= max && !counted) {
+				result++;
+				counted = true;
+				return;
+			}
+		});
+	});
 
 	return result;
 };
