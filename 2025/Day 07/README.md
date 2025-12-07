97ec49b88085a018a7ae3cc745522715c8cbbe03
Stanisław Synowiec <stasyn1410@gmail.com>
2025-12-07 06:51:58 +0100

    Day 07 - part one 2025

--- 2025/Day 07/README.md
+++ 2025/Day 07/README.md
@@ -1,12 +1,12 @@
-# --- Day 07: ---
+# --- Day 07: Laboratories ---
 
 ## [Source](http://adventofcode.com/2025/day/7)
 
 | Name                                                                                                | Correctness    |
 | :-------------------------------------------------------------------------------------------------- | :------------- |
-| [Part One](https://github.com/ssynowiec/AdventOfCode/blob/main/2025/Day%2007/part-one.ts)           | in progress... |
+| [Part One](https://github.com/ssynowiec/AdventOfCode/blob/main/2025/Day%2007/part-one.ts)           | Correct ✅     |
 | [Part Two](https://github.com/ssynowiec/AdventOfCode/blob/main/2025/Day%2007/part-two.ts)           | in progress... |
-| [Test Part One](https://github.com/ssynowiec/AdventOfCode/blob/main/2025/Day%2007/index.test.ts#L7) | in progress... |
+| [Test Part One](https://github.com/ssynowiec/AdventOfCode/blob/main/2025/Day%2007/index.test.ts#L7) | Correct ✅     |
 | [Test Part Two](https://github.com/ssynowiec/AdventOfCode/blob/main/2025/Day%200/index.test.ts#L19) | in progress... |
 
-### Day n progress...
+### Day complieted with ⭐
--- 2025/Day 07/part-one.ts
+++ 2025/Day 07/part-one.ts
@@ -1,11 +1,69 @@
 import { readFile } from '@/helpers/read-file';
 
+const findAllIndexs = (arr: string[], find: string) => {
+	const indexs = [];
+
+	let index = -1;
+	while ((index = arr.indexOf(find, index + 1)) !== -1) {
+		indexs.push(index);
+	}
+
+	return indexs;
+};
+
 export const partOne = (input: string): number => {
-	const data = readFile(input);
+	const data = readFile(input).map(line => line.split(''));
 
 	let result = 0;
 
-	data.forEach(line => {});
+	const startPosition = data[0].indexOf('S');
+
+	const path: string[][] = [];
+	data.forEach((line, i) => {
+		if (i > 1) {
+			const pathsPositons = findAllIndexs(path[i - 1], '|');
+
+			if (line.includes('^')) {
+				const splitterPositons = findAllIndexs(line, '^');
+
+				const splittedPath = [];
+
+				for (let i = 0; i < line.length; i++) {
+					if (
+						splitterPositons.includes(i) &&
+						pathsPositons.includes(i)
+					) {
+						splittedPath[i - 1] = '|';
+						splittedPath[i + 1] = '|';
+						splittedPath[i] = line[i];
+						i++;
+						result++;
+					} else {
+						if (pathsPositons.includes(i)) {
+							splittedPath[i] = '|';
+						} else {
+							splittedPath[i] = line[i];
+						}
+					}
+				}
+				path.push(splittedPath);
+			} else {
+				const continuePaths = line.map((_, pos) =>
+					pathsPositons.includes(pos) ? '|' : '.',
+				);
+				path.push(continuePaths);
+			}
+		} else {
+			if (line.includes('S')) {
+				path.push(line);
+				return;
+			}
+			const continuePaths = line.map((_, pos) =>
+				startPosition === pos ? '|' : '.',
+			);
+			path.push(continuePaths);
+		}
+	});
 
 	return result;
 };
